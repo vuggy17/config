d2d659142fcc320c98b4ac2eab11474e572494d2
Vuggy <61084329+vuggy17@users.noreply.github.com>
2023-07-04 16:24:12 +0700

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,12 @@
+starship init fish | source
+
+# Aliases
+alias rm="rm -i"
+alias fconfig="nano ~/.config/fish/config.fish"
+alias projects="cd /e/projects"
+alias here="explorer.exe ."
+alias reload='source ~/.config/fish/config.fish'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
